785e9e70ba8b042ab50f8d6c62ab4ad004ecc94c
wendiang <371174590@qq.com>
2017-12-03 23:02:37 +0800

    add

---
+++ food.sql
@@ -0,0 +1,22 @@
+﻿# Host: localhost  (Version: 5.5.47)
+# Date: 2017-12-03 23:01:53
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
